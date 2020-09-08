362752d2ca7d576dfa6a9683fb4c632c4194f2a8
MrbeastmodeXD <70970191+MrbeastmodeXD@users.noreply.github.com>
2020-09-08 13:03:11 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+licensekey = '',
+info = {
+        'name': 'Test Profile',
+        'email': 'testguy123@gmail.com',
+        'phone': '123-456-7890',
+        'ccnumber': '4242424242424242',
+        'ccexpirymonth': '12',
+        'ccexpiryyear': '25',
+        'cccvc': '123',
+        'address1': '123 test street',
+        'address2': 'apt 123',
+        'zipcode': '90210',
+        'city': 'Beverly Hills',
+        'gmail_login': '',
+        'gmail_password': '',
+}
+        
